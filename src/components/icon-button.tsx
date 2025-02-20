04a130fc21f94e48e883ec0afce5080489d1bcf3
Gustavo Mazzaro <gustamazzaro@gmail.com>
2025-02-20 11:53:45 -0300

    Criando pagina de invite

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -1,11 +1,15 @@
 import type { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
 
 interface iconButtonprops extends ComponentProps<"button"> {}
 
-export function IconButton(props: iconButtonprops) {
+export function IconButton({ className, ...props }: iconButtonprops) {
   return (
     <button
-      className="p-1.5 bg-gray-500 text-blue  rounded-md cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-300"
+      className={twMerge(
+        "p-1.5 bg-gray-500 text-blue  rounded-md cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-300",
+        className
+      )}
       {...props}
     />
   );
